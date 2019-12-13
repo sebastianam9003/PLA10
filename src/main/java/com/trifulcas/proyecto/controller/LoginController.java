b41280d965409f7d21721733dcf698b3d9e4baac
sebastianam9003 <56412154+sebastianam9003@users.noreply.github.com>
2019-12-13 18:22:02 +0100

    Add files via upload

--- src/main/java/com/trifulcas/proyecto/controller/LoginController.java
+++ src/main/java/com/trifulcas/proyecto/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.trifulcas.proyecto.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class LoginController {
 
 	@GetMapping("/formlogin")
